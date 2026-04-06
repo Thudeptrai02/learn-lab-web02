d399810e6ab0f3dd8bf7f26449d2369d1485f508
Luong Xuan Thu <lxthu02@gmail.com>
2026-04-06 15:47:58 +0700

    Cai dat Vercel Adapter fix loi

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://learnlab.vn', // <--- SẾP THÊM ĐÚNG DÒNG NÀY VÀO ĐÂY NHÉ
+  // <--- SẾP THÊM ĐÚNG DÒNG NÀY VÀO ĐÂY NHÉ
+  site: 'https://learnlab.vn',
+
   output: 'static',
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  adapter: vercel()
 });
